165114f71dd7c8f140c6aeb12b807b6247a095fc
Oleg Karpov <berelianian@gmail.com>
2018-01-10 17:23:03 +0200

    redmine project & task get or create implementation

--- redmine_stat/models/models.py
+++ redmine_stat/models/models.py
@@ -28,55 +28,57 @@ class RedmineTimeEntry(orm.Model):
         from redminelib import Redmine
         redmine = Redmine('https://pm.syntech.software', username='oleg.karpov@syntech.software', password='123456')
         for project in redmine.project.all():
-            proj_obj = self.env['project.project'].create({'name': project.name, 'allow_timesheets': True})
+            if not self.env['project.project'].search([('name', '!=', project.name)]):
+                proj_obj = self.env['project.project'].create({'name': project.name, 'allow_timesheets': True})
 
-
-            tasks = []
-            try:
-                tasks = list(project.issues)
-            except Exception:
-                pass
-
-            for task in tasks:
-                task_obj = self.env['project.task'].create({
-                    'project_id': proj_obj.id,
-                    'name': task,
-                    'description': task.description,
-                    # 'priority': task.priority,
-                    'create_date': task.created_on,
-                    # 'date_start': task.start_date,
-                    # 'date_end': task.start_end,
-                    # 'date_deadline': task.due_date,
-                    # 'date_last_stage_update': task.updated_on,
-                    'progress': task.done_ratio,
-                    # 'status': task.status,
-                })
-
-                time_entrys = []
+                tasks = []
                 try:
-                    time_entrys = list(task.time_entries)
+                    tasks = list(project.issues)
                 except Exception:
                     pass
 
-                for time_entry in time_entrys:
-                    if not self.search([('redmine_id', '=', time_entry.id)]):
+                for task in tasks:
+                    if not self.env['project.project'].search([('name', '!=', str(task)), ('project_id', '!=', project.id)]):
 
-                        time_entry_name = time_entry.comments or str(time_entry)
-                        timesheet_obj = self.env['account.analytic.line'].create({
-                            'name': time_entry_name,
+                        task_obj = self.env['project.task'].create({
                             'project_id': proj_obj.id,
-                            'task_id': task_obj.id
+                            'name': str(task),
+                            'description': task.description,
+                            # 'priority': task.priority,
+                            'create_date': task.created_on,
+                            # 'date_start': task.start_date,
+                            # 'date_end': task.start_end,
+                            # 'date_deadline': task.due_date,
+                            # 'date_last_stage_update': task.updated_on,
+                            'progress': task.done_ratio,
+                            # 'status': task.status,
                         })
 
-                        self.create({
-                            'timesheet_id': timesheet_obj.id,
-                            'redmine_id': time_entry.id,
-                            'sync_date': datetime.now(),
-                            'updated_on_redmine': datetime.now(),
-                            'name': time_entry.comments,
-                            'create_date': time_entry.created_on,
-                            'amount': time_entry.hours,
-                        })
+                        time_entrys = []
+                        try:
+                            time_entrys = list(task.time_entries)
+                        except Exception:
+                            pass
+
+                        for time_entry in time_entrys:
+                            if self.search([('redmine_id', '!=', time_entry.id)]):
+
+                                time_entry_name = time_entry.comments or str(time_entry)
+                                timesheet_obj = self.env['account.analytic.line'].create({
+                                    'name': time_entry_name,
+                                    'project_id': proj_obj.id,
+                                    'task_id': task_obj.id
+                                })
+
+                                self.create({
+                                    'timesheet_id': timesheet_obj.id,
+                                    'redmine_id': time_entry.id,
+                                    'sync_date': datetime.now(),
+                                    'updated_on_redmine': datetime.now(),
+                                    'name': time_entry.comments,
+                                    'create_date': time_entry.created_on,
+                                    'amount': time_entry.hours,
+                                })
 
         return super(RedmineTimeEntry, self).write(vals)
 
